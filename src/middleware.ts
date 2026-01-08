c5a575b1339e5aa4d0aee59e81ea6c3329458afd
MeerMuneeb <jimmykuddon@gmail.com>
2026-01-08 23:07:14 +0500

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,38 @@
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-
-  // Redirect root only
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/auth/sign-in', request.url))
-  }
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Run middleware on everything EXCEPT:
-     * - auth routes
-     * - api routes
-     * - next internals
-     * - static files
-     */
-    '/((?!auth|api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/((?!api|_next|favicon.ico).*)'],
 }
+
+
+// // middleware.ts
+// import { NextRequest, NextResponse } from 'next/server'
+
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl
+
+//   // Redirect root to org user sign-in
+//   if (pathname === '/') {
+//     return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+//   }
+
+//   return NextResponse.next()
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      * - public files (images, etc)
+//      */
+//     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+//   ],
+// }
